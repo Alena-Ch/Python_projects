a93f44cfaf1e9a28fadf116626a157e15e95c335
Alena <alenaryzhko1@gmail.com>
2020-06-22 16:42:07 -0400

    changes added

--- best_buy/features/environment.py
+++ best_buy/features/environment.py
@@ -8,7 +8,7 @@ def browser_init(context):
     :param context: Behave context
     """
     context.driver = webdriver.Chrome(executable_path='drivers/chromedriver')
-    # context.driver = webdriver.Firefox(executable_path='drivers/geckodriver')
+    #context.driver = webdriver.Firefox(executable_path='drivers/geckodriver')
     # context.driver = webdriver.Safari()
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
